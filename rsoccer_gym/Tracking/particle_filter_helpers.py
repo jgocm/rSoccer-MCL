f8f2c4b9eb52eb9b60ba0e9c7ac6b53a2562ad4c
joao guilherme melo <jgocm@cin.ufpe.br>
2023-05-11 18:38:58 -0300

    adds movement methods to helper

--- rsoccer_gym/Tracking/particle_filter_helpers.py
+++ rsoccer_gym/Tracking/particle_filter_helpers.py
@@ -119,3 +119,20 @@ def is_out_of_field(particle_state, x_min, x_max, y_min, y_max):
         return True
     else:
         return False
+    
+def rotate_to_global(local_x, local_y, robot_w):
+    theta = np.deg2rad(robot_w)
+    global_x = local_x*np.cos(theta) - local_y*np.sin(theta)
+    global_y = local_x*np.sin(theta) + local_y*np.cos(theta)
+    return global_x, global_y
+
+def add_move_noise(movement, deviation):
+    standard_deviation_vector = np.abs(movement)*deviation
+    return np.random.normal(movement, standard_deviation_vector, 3)
+
+def limit_angle_degrees(angle):
+    while angle > 180:
+        angle -= 2*180
+    while angle < -180:
+        angle += 2*180
+    return angle
