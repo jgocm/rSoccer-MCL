016f172118c584297eb243e51b9199c625b96b3e
jgocm <jgocm@cin.ufpe.br>
2023-06-21 18:28:32 -0300

    split recv and send classes

--- rsoccer_gym/Utils/mcl_communication.py
+++ rsoccer_gym/Utils/mcl_communication.py
@@ -2,41 +2,17 @@ import socket
 import numpy as np
 from particles_pb2 import *
 
-class SocketUDP():
-    def __init__(
-                self,
-                host_address='192.168.0.193',
-                host_port=9601,
-                device_address='192.168.0.123',
-                device_port=9600                
-                ):
-        super(SocketUDP, self).__init__()
-        self.host_address = host_address
-        self.host_port = host_port
-        self.device_address = device_address
-        self.device_port = device_port
+class ParticlesReceiver():
+    def __init__(self,
+                 sender_address = '192.168.0.193',
+                 port = 9600): 
+        self.address = sender_address
+        self.port = port
         self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udp_sock.bind(('', self.host_port))
+        self.udp_sock.bind(('', self.port))
         self.udp_sock.settimeout(0)
         self.msg = ParticleList()
 
-    def setHostUDP(self, address, port):
-        self.host_address=address
-        self.host_port=port
-    
-    def setDeviceUDP(self, address,port):
-        self.device_address=address
-        self.device_port=port
-
-    def setMCLMessage(self, particles):
-        self.msg = ParticleList()
-        for particle in particles:
-            p = self.msg.particles.add()
-            p.w, p.x, p.y, p.theta = particle
-
-    def sendMCLMessage(self):
-        self.udp_sock.sendto(self.msg.SerializeToString(), (self.device_address, self.device_port))
-    
     def recvMCLMessage(self):
         msg = ParticleList()
 
@@ -56,39 +32,53 @@ class SocketUDP():
             particles.append(p)
         
         return np.array(particles)
+    
+class ParticlesSender():
+    def __init__(self,
+                 receiver_address='192.168.0.123',
+                 port=9600):
+        self.address = receiver_address
+        self.port = port
+        self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.msg = ParticleList()
 
-if __name__ == "__main__":
+    def setMCLMessage(self, particles):
+        self.msg = ParticleList()
+        for particle in particles:
+            p = self.msg.particles.add()
+            p.w, p.x, p.y, p.theta = particle
 
-    host_address = "192.168.0.193"
-    host_port = 9601
-    device_address = "192.168.0.123"
-    device_port = 9600
+    def sendMCLMessage(self):
+        self.udp_sock.sendto(self.msg.SerializeToString(), (self.address, self.port))
+    
 
-    UDP = SocketUDP(
-        host_address=host_address,
-        host_port=host_port,
-        device_address=device_address,
-        device_port=device_port
-    )
+def run_receiver():
+    UDP = ParticlesReceiver()
 
-    is_receiver = True
-
-    # RECEIVER:
-    while is_receiver:
+    while True:
         particles = UDP.recvMCLMessage()
         # Print the list of particles
         for particle in particles:
-            print(particle)
-    
-    # SENDER:
+            print(particle)  
+
+def run_sender():
+    UDP = ParticlesSender()
+
     particles = np.array([
         [1.0, 2.0, 3.0, 0.5],
         [2.0, 3.0, 4.0, 1.0],
         [3.0, 4.0, 5.0, 1.5]
     ])
     UDP.setMCLMessage(particles)
-    while not is_receiver:
+    while True:
         UDP.sendMCLMessage()
+
+if __name__ == "__main__":
+
+    is_receiver = False
+
+    if is_receiver: run_receiver()
+    else: run_sender()
 
 
 
